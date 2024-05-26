ca6bc3169720c72360a1c8c0b31ae0e92785ce6c
warramma <wishmorethewitch@gmail.com>
2024-05-26 12:09:24 -0500

    adding a limit to guesses

--- guessing_game.py
+++ guessing_game.py
@@ -13,10 +13,17 @@ def print_hints():
 print_hints()
 
 guess = input('What is the SECRET WORD? ')
+limit = 3
+i = 1
 
-while not(guess.lower() == 'rose'):
-    print('Oops! Guess again!')
+while not(guess.lower() == 'rose') and i < limit:
+    print('Oops! Guess again! You have ' + str(limit - i) + ' tries left :)' )
+    i += 1
     print_hints()
     guess = input('What is the SECRET WORD? ')
 
-print('You\'ve done it! You\'ve figured out the SECRET WORD!!')
+
+if i == limit:
+    print('Better luck next time :(')
+else:
+    print('You\'ve done it! You\'ve figured out the SECRET WORD!!')
