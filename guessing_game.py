1bc6a9c905604c120f76054ca6d76f5b853091a7
warramma <wishmorethewitch@gmail.com>
2024-05-26 12:04:52 -0500

    create simple guessing game

--- guessing_game.py
+++ guessing_game.py
@@ -2,4 +2,21 @@
 
 #guessing game - try and guess the secret word
 
-print('Try and guess the SECRET WORD!')
+print('Try and guess the SECRET WORD! ')
+
+secret_word = 'rose'
+
+def print_hints():
+    print('Hint 1: a flower')
+    print('Hint 2: very popular')
+
+print_hints()
+
+guess = input('What is the SECRET WORD? ')
+
+while not(guess.lower() == 'rose'):
+    print('Oops! Guess again!')
+    print_hints()
+    guess = input('What is the SECRET WORD? ')
+
+print('You\'ve done it! You\'ve figured out the SECRET WORD!!')
